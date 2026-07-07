4f0dd5a884a64a0e08a8b6a9ae12e85ab41e0e9b
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2026-07-06 21:30:56 -0500

    Agregar implementación de carga de energía en la función main y definir clases WindPlant y WaterPlant

--- 09-abstract-class.dart
+++ 09-abstract-class.dart
@@ -1,7 +1,20 @@
-void main() {}
+void main() {
+  final windPlant = WindPlant(initialEnergy: 9);
+  print('wind ${chargePhone(windPlant)}');
+}
 
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy to charge the phone');
+  }
+
+  return plant.energyLeft - 10;
+}
+
+// Definición de un ENUM
 enum PlantType { nuclear, wind, water }
 
+// Clase Abstracta
 abstract class EnergyPlant {
   double energyLeft;
   PlantType type;
@@ -9,4 +22,29 @@ abstract class EnergyPlant {
   EnergyPlant({required this.energyLeft, required this.type});
 
   void consumeEnergy(double amount);
+}
+
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+    : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
+
+class WaterPlant extends EnergyPlant {
+  WaterPlant({required double energyLeft})
+    : super(energyLeft: energyLeft, type: PlantType.water);
+
+  @override
+  void consumeEnergy(double amount) {
+    if (energyLeft >= amount) {
+      energyLeft -= amount;
+      print('Water plant consumed $amount energy. Energy left: $energyLeft');
+    } else {
+      print('Not enough energy in water plant.');
+    }
+  }
 }
