15dbed6a4f7b9205b4425711a5be1f8eafc10c20
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2026-07-06 21:50:12 -0500

    feat: mixins

---
+++ 10-mixins.dart
@@ -0,0 +1,46 @@
+/**
+ * Sirve para especializar a la clase en algo 
+ */
+
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+
+abstract class Ave extends Animal {}
+
+abstract class Pez extends Animal {}
+
+mixin class Volador {
+  void volar() => print('Estoy volando');
+}
+
+mixin class Caminante {
+  void caminar() => print('Estoy caminando');
+}
+
+mixin class Nadador {
+  void nadar() => print('Estoy nadando');
+}
+
+class Delfin extends Mamifero with Nadador {}
+
+class Murcielago extends Mamifero with Volador, Caminante {}
+
+class Gato extends Mamifero with Caminante {}
+
+class Paloma extends Ave with Volador, Caminante {}
+
+class Pato extends Ave with Nadador, Caminante, Volador {}
+
+class Tiburon extends Pez with Nadador {}
+
+class PezVolador extends Pez with Nadador, Volador {}
+
+void main() {
+  final flipper = Delfin();
+  flipper.nadar();
+
+  final batman = Murcielago();
+  batman.caminar();
+  batman.volar();
+}
